e7f05979daf9c4109cee0b91e850920f6f068d8a
Mehdi Rafiei <mrafee113@gmail.com>
2026-08-22 18:23:59 +0330

    daily 2026-08-22: 3622

---
+++ leetcode/3622.check-divisibility-by-digit-sum-and-product.py
@@ -0,0 +1,66 @@
+# Category: algorithms
+# Level: Easy
+# Percent: 69.69096%
+
+
+
+# You are given a positive integer n. Determine whether n is divisible by the sum of the following two values:
+# 
+# 
+# 	
+# 	The digit sum of n (the sum of its digits).
+# 	
+# 	
+# 	The digit product of n (the product of its digits).
+# 	
+# 
+# 
+# Return true if n is divisible by this sum; otherwise, return false.
+# 
+#  
+# Example 1:
+# 
+# 
+# Input: n = 99
+# 
+# Output: true
+# 
+# Explanation:
+# 
+# Since 99 is divisible by the sum (9 + 9 = 18) plus product (9 * 9 = 81) of its digits (total 99), the output is true.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: n = 23
+# 
+# Output: false
+# 
+# Explanation:
+# 
+# Since 23 is not divisible by the sum (2 + 3 = 5) plus product (2 * 3 = 6) of its digits (total 11), the output is false.
+# 
+# 
+#  
+# Constraints:
+# 
+# 
+# 	1 <= n <= 10⁶
+# 
+ 
+
+# CODE-START
+class Solution:
+    def checkDivisibility(self, n: int) -> bool:
+        t = n
+        s = 0
+        m = 1
+        while t > 0:
+            t, d = divmod(t, 10)
+            m *= d
+            s += d
+        
+        return n % (s + m) == 0
+        
+# CODE-END
