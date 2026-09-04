60cbdd36d7f3d772e661fff1455a75de93d04584
Mehdi Rafiei <mrafee113@gmail.com>
2026-09-04 22:19:50 +0330

    neetcode 150: 763

---
+++ leetcode/763.partition-labels.py
@@ -0,0 +1,53 @@
+# Category: algorithms
+# Level: Medium
+# Percent: 81.96893%
+
+
+
+# You are given a string s. We want to partition the string into as many parts as possible so that each letter appears in at most one part. For example, the string "ababcc" can be partitioned into ["abab", "cc"], but partitions such as ["aba", "bcc"] or ["ab", "ab", "cc"] are invalid.
+# 
+# Note that the partition is done so that after concatenating all the parts in order, the resultant string should be s.
+# 
+# Return a list of integers representing the size of these parts.
+# 
+#  
+# Example 1:
+# 
+# Input: s = "ababcbacadefegdehijhklij"
+# Output: [9,7,8]
+# Explanation:
+# The partition is "ababcbaca", "defegde", "hijhklij".
+# This is a partition so that each letter appears in at most one part.
+# A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+# 
+# 
+# Example 2:
+# 
+# Input: s = "eccbbbbdec"
+# Output: [10]
+# 
+# 
+#  
+# Constraints:
+# 
+# 
+# 	1 <= s.length <= 500
+# 	s consists of lowercase English letters.
+# 
+ 
+
+# CODE-START
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        rndx = {c: i for i, c in enumerate(s)}
+        l = r = 0
+        o = list()
+        for i, c in enumerate(s):
+            r = max(r, rndx[c])
+            if i == r:
+                o.append(i - l + 1)
+                l = i + 1
+
+        return o
+        
+# CODE-END
