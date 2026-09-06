e942322387e1e723e4c0df2f148d509137e2e5c2
Mehdi Rafiei <mrafee113@gmail.com>
2026-09-06 09:39:18 +0330

    neetcode 150: 312

---
+++ leetcode/312.burst-balloons.py
@@ -0,0 +1,54 @@
+# Category: algorithms
+# Level: Hard
+# Percent: 63.947563%
+
+
+
+# You are given n balloons, indexed from 0 to n - 1. Each balloon is painted with a number on it represented by an array nums. You are asked to burst all the balloons.
+# 
+# If you burst the ith balloon, you will get nums[i - 1] * nums[i] * nums[i + 1] coins. If i - 1 or i + 1 goes out of bounds of the array, then treat it as if there is a balloon with a 1 painted on it.
+# 
+# Return the maximum coins you can collect by bursting the balloons wisely.
+# 
+#  
+# Example 1:
+# 
+# Input: nums = [3,1,5,8]
+# Output: 167
+# Explanation:
+# nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
+# coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167
+# 
+# Example 2:
+# 
+# Input: nums = [1,5]
+# Output: 10
+# 
+# 
+#  
+# Constraints:
+# 
+# 
+# 	n == nums.length
+# 	1 <= n <= 300
+# 	0 <= nums[i] <= 100
+# 
+ 
+
+# CODE-START
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        nums = [1] + list(filter(int, nums)) + [1]
+        n = len(nums)
+        dp = [[0] * n for _ in range(n)]
+        for size in range(3, n + 1):
+            for i in range(n - size + 1):
+                j = i + size - 1
+                ij = nums[i] * nums[j]
+                dp[i][j] = max([
+                    dp[i][k] + ij * nums[k] + dp[k][j]
+                    for k in range(i + 1, j)
+                ])
+        return dp[0][n - 1]
+        
+# CODE-END
