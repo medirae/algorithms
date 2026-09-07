de0a1f8ed35412226a2ea8453043814fd9ef9bbc
Mehdi Rafiei <mrafee113@gmail.com>
2026-09-07 08:34:27 +0330

    neetcode 150: 332

---
+++ leetcode/332.reconstruct-itinerary.py
@@ -0,0 +1,98 @@
+# Category: algorithms
+# Level: Hard
+# Percent: 44.773853%
+
+
+
+# You are given a list of airline tickets where tickets[i] = [fromi, toi] represent the departure and the arrival airports of one flight. Reconstruct the itinerary in order and return it.
+# 
+# All of the tickets belong to a man who departs from "JFK", thus, the itinerary must begin with "JFK". If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string.
+# 
+# 
+# 	For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+# 
+# 
+# You may assume all tickets form at least one valid itinerary. You must use all the tickets once and only once.
+# 
+#  
+# Example 1:
+# 
+# Input: tickets = [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]
+# Output: ["JFK","MUC","LHR","SFO","SJC"]
+# 
+# 
+# Example 2:
+# 
+# Input: tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+# Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
+# Explanation: Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"] but it is larger in lexical order.
+# 
+# 
+#  
+# Constraints:
+# 
+# 
+# 	1 <= tickets.length <= 300
+# 	tickets[i].length == 2
+# 	fromi.length == 3
+# 	toi.length == 3
+# 	fromi and toi consist of uppercase English letters.
+# 	fromi != toi
+# 
+ 
+
+# CODE-START
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        return self.sorting(tickets)
+        return self.pq(tickets)
+
+    @staticmethod
+    def sorting(tickets):
+        g = dict()
+        for u, v in tickets:
+            if u not in g:
+                g[u] = list()
+            
+            if v not in g:
+                g[v] = list()
+            
+            g[u].append(v)
+        
+        for u, v in g.items():
+            v.sort(reverse=True)
+
+        o = list()
+        def dfs(node="JFK"):
+            while g[node]:
+                dfs(g[node].pop())
+            
+            o.append(node)
+            return o
+        
+        return dfs()[::-1]
+
+    @staticmethod
+    def pq(tickets):
+        g = dict()
+        for u, v in tickets:
+            if u not in g:
+                g[u] = list()
+            
+            if v not in g:
+                g[v] = list()
+
+            heapq.heappush(g[u], v)
+
+        o = list()
+        def dfs(node="JFK"):
+            h = g[node]
+            while h:
+                dfs(heapq.heappop(h))
+
+            o.append(node)
+            return o
+
+        return dfs()[::-1]
+        
+# CODE-END
