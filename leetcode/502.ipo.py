f330127b7f6ecc8b3c5c5c6e874a3037fb9b9d4f
Mehdi Rafiei <mrafee113@gmail.com>
2026-09-18 20:08:37 +0330

    neetcode 250: 502

---
+++ leetcode/502.ipo.py
@@ -0,0 +1,70 @@
+# Category: algorithms
+# Level: Hard
+# Percent: 53.634422%
+
+
+
+# Suppose LeetCode will start its IPO soon. In order to sell a good price of its shares to Venture Capital, LeetCode would like to work on some projects to increase its capital before the IPO. Since it has limited resources, it can only finish at most k distinct projects before the IPO. Help LeetCode design the best way to maximize its total capital after finishing at most k distinct projects.
+# 
+# You are given n projects where the ith project has a pure profit profits[i] and a minimum capital of capital[i] is needed to start it.
+# 
+# Initially, you have w capital. When you finish a project, you will obtain its pure profit and the profit will be added to your total capital.
+# 
+# Pick a list of at most k distinct projects from given projects to maximize your final capital, and return the final maximized capital.
+# 
+# The answer is guaranteed to fit in a 32-bit signed integer.
+# 
+#  
+# Example 1:
+# 
+# Input: k = 2, w = 0, profits = [1,2,3], capital = [0,1,1]
+# Output: 4
+# Explanation: Since your initial capital is 0, you can only start the project indexed 0.
+# After finishing it you will obtain profit 1 and your capital becomes 1.
+# With capital 1, you can either start the project indexed 1 or the project indexed 2.
+# Since you can choose at most 2 projects, you need to finish the project indexed 2 to get the maximum capital.
+# Therefore, output the final maximized capital, which is 0 + 1 + 3 = 4.
+# 
+# 
+# Example 2:
+# 
+# Input: k = 3, w = 0, profits = [1,2,3], capital = [0,1,2]
+# Output: 6
+# 
+# 
+#  
+# Constraints:
+# 
+# 
+# 	1 <= k <= 10⁵
+# 	0 <= w <= 10⁹
+# 	n == profits.length
+# 	n == capital.length
+# 	1 <= n <= 10⁵
+# 	0 <= profits[i] <= 10⁴
+# 	0 <= capital[i] <= 10⁹
+# 
+ 
+
+# CODE-START
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: list[int], capital: list[int]) -> int:
+        if w >= max(capital):
+            return w + sum(heapq.nlargest(k, profits))
+
+        prjs = sorted((capital[ndx], profits[ndx]) for ndx in range(len(profits)) if profits[ndx] > 0)
+        ndx, n = 0, len(prjs)
+        q = list()
+        while k:
+            while ndx < n and prjs[ndx][0] <= w:
+                heapq.heappush(q, -prjs[ndx][1])
+                ndx += 1
+
+            if not q:
+                break
+            
+            w += -heapq.heappop(q)
+            k -= 1
+
+        return w
+# CODE-END
